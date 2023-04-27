1fe7d80e2d6d7f8e09b22f78c1241bd5bbef2348
= <=>
2023-04-27 16:05:41 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,13 +8,97 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def ridge(data):
-    pass
+def ridge(x,y):
+    alpha=0.2; #设置超参数alpha
+    xTx=np.dot(x.T,x)
+    rxTx=xTx+np.eye(x.shape[1])*alpha
+    weight=np.dot(np.linalg.inv(rxTx),np.dot(x.T,y))
+    return weight
     
-def lasso(data):
-    pass
+
+
+def lasso(X, y):
+    """
+    Lasso回归，使用坐标下降法（coordinate descent）
+    args:
+        X - 训练数据集
+        y - 目标标签值
+        lambdas - 惩罚项系数
+        max_iter - 最大迭代次数
+        tol - 变化量容忍值
+    return:
+        w - 权重系数
+    """
+    lambdas=0.1
+    max_iter=1000
+    tol=1e-4
+    w = np.zeros(X.shape[1])
+    for it in range(max_iter):
+        done = True
+        # 遍历所有自变量
+        for i in range(0, len(w)):
+            # 记录上一轮系数
+            weight = w[i]
+            # 求出当前条件下的最佳系数
+            w[i] = down(X, y, w, i, lambdas)
+            # 当其中一个系数变化量未到达其容忍值，继续循环
+            if (np.abs(weight - w[i]) > tol):
+                done = False
+        # 所有系数都变化不大时，结束循环
+        if (done):
+            break
+    return w
+
+def down(X, y, w, index, lambdas=0.1):
+    """
+    cost(w) = (x1 * w1 + x2 * w2 + ... - y)^2 + ... + λ(|w1| + |w2| + ...)
+    假设 w1 是变量，这时其他的值均为常数，带入上式后，其代价函数是关于 w1 的一元二次函数，可以写成下式：
+    cost(w1) = (a * w1 + b)^2 + ... + λ|w1| + c (a,b,c,λ 均为常数)
+    => 展开后
+    cost(w1) = aa * w1^2 + 2ab * w1 + λ|w1| + c (aa,ab,c,λ 均为常数)
+    """
+    # 展开后的二次项的系数之和
+    aa = 0
+    # 展开后的一次项的系数之和
+    ab = 0
+    for i in range(X.shape[0]):
+        # 括号内一次项的系数
+        a = X[i][index]
+        # 括号内常数项的系数
+        b = X[i][:].dot(w) - a * w[index] - y[i]
+        # 可以很容易的得到展开后的二次项的系数为括号内一次项的系数平方的和
+        aa = aa + a * a
+        # 可以很容易的得到展开后的一次项的系数为括号内一次项的系数乘以括号内常数项的和
+        ab = ab + a * b
+    # 由于是一元二次函数，当导数为零时，函数值最小值，只需要关注二次项系数、一次项系数和 λ
+    return det(aa, ab, lambdas)
+
+def det(aa, ab, lambdas=0.1):
+    """
+    通过代价函数的导数求 w，当 w = 0 时，不可导
+    det(w) = 2aa * w + 2ab + λ = 0 (w > 0)
+    => w = - (2 * ab + λ) / (2 * aa)
+
+    det(w) = 2aa * w + 2ab - λ = 0 (w < 0)
+    => w = - (2 * ab - λ) / (2 * aa)
+
+    det(w) = NaN (w = 0)
+    => w = 0
+    """
+    w = - (2 * ab + lambdas) / (2 * aa)
+    if w < 0:
+        w = - (2 * ab - lambdas) / (2 * aa)
+        if w > 0:
+            w = 0
+    return w
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+import numpy as np
+
+x,y=read_data('C:\\Users\\Lenovo\\hello-world-y1zqianbb\\linear-regression-y1zqianbb\\data\\exp02\\')
+print(ridge(x,y))
+print("//////////////////////")
+print(lasso(x,y))
